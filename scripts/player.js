5edae663ce3b752e7f79e8ce94372c569c958a94
Lex_TM <BenuelBertran@gmail.com>
2018-12-26 12:55:45 +0300

    add to gitHub

---
+++ scripts/player.js
@@ -0,0 +1,53 @@
+/* jshint node: true */
+/* jshint browser: true */
+/* global window */
+"use strict";
+
+window.addEventListener("load", function() {
+  const album = document.querySelector(".player__album");
+  const albumButton = document.querySelector(".btn__album");
+  const albumTitle = document.querySelector(".album__title");
+  const albumBorder = document.querySelector(".album__border");
+  const play = document.querySelector(".play");
+  const playButton = document.querySelector(".player__play-button");
+  const playButtonTitle = document.querySelector(".btn__title");
+  const songs = document.querySelector(".player__songs-list");
+  const songsListButton = document.querySelector(".btn__songs");
+  const songsTitle = document.querySelector(".songs__title");
+  const songsAdd = document.querySelector(".songs__add");
+  
+  
+  albumButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    album.classList.toggle("hidden");
+  });
+  
+  songsListButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    songs.classList.toggle("hidden");
+  });
+  
+  play.addEventListener("click", function () {
+    if (!play.classList.contains("active")) {
+      playButton.classList.add("sssr-btn");
+      album.classList.add("sssr-album");
+      songs.classList.add("sssr-songs");
+      playButtonTitle.innerText = "";
+      albumTitle.style = "visibility: hidden;";
+      songsTitle.style = "visibility: hidden;";
+      albumBorder.style = "visibility: hidden;";
+      songsAdd.style = "visibility: hidden;";
+    }
+    else {
+      playButton.classList.remove("sssr-btn");
+      album.classList.remove("sssr-album");
+      songs.classList.remove("sssr-songs");
+      playButtonTitle.innerText = "PLAY MUSIC";
+      albumTitle.style = "visibility: none;";
+      songsTitle.style = "visibility: none;";
+      albumBorder.style = "visibility: none;";
+      songsAdd.style = "visibility: none;";
+    }
+  });
+});
+
---
+++ scripts/player.jsx
@@ -0,0 +1,70 @@
+const PlayButton = (props) => {
+  const className = props.isMusicPlaying ? "play active" : "play";
+  return (
+    <a onClick={props.onClick} className={className} href="#" title="Play music"></a>
+  )
+}
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMusicPlaying: false };
+  }
+
+  onPlayButtonClick(evt) {
+    if (this.state.isMusicPlaying) {
+      this.audio.pause();
+    } else {
+      this.audio.play();
+    }
+    this.setState(prevState => {
+      return { 
+        isMusicPlaying: !prevState.isMusicPlaying   
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="btn player__play-button">
+        <PlayButton 
+          onClick={ this.onPlayButtonClick.bind(this) }
+          isMusicPlaying={ this.state.isMusicPlaying }
+        />
+        <audio className="btn__audio" src="https://benuelbertran.github.io/spandexmusic/music/YourJoy.mp3" ref={(audioTag) => { this.audio = audioTag }}/>
+        <h2 className="btn__title">Play Music</h2>
+        <button className="btn__album"></button>
+        <button className="btn__songs"></button> 
+      </div>
+    );
+  }
+}
+
+const Album = (props) => {
+  return (
+    <div className="album player__album hidden">
+      <h2 className="album__title">Album<br/> cover</h2>
+      <div className="album__border">
+        <p className="album__add">Add picture here</p>
+      </div>
+    </div>
+  )
+}
+
+const SongsList = (props) => {
+  return (
+    <div className="songs player__songs-list hidden">
+      <h2 className="songs__title">Songs<br/> list</h2>
+      <p className="songs__add">Drag'N'Drop<br/> your music inside</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.Fragment>
+    <Album />
+    <Container />
+    <SongsList />
+  </React.Fragment>,
+  document.querySelector(".player")
+);
